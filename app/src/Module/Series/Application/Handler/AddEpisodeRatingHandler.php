912c639ae73fc2b3d8b4da10c3fc7c854f1e62fa
Leszek Koziatek <poczta@leszekkoziatek.pl>
2026-04-26 20:35:36 +0200

    HMAI-12 - Graylog + New Relic — centralized logging and APM

--- app/src/Module/Series/Application/Handler/AddEpisodeRatingHandler.php
+++ app/src/Module/Series/Application/Handler/AddEpisodeRatingHandler.php
@@ -7,6 +7,7 @@ namespace App\Module\Series\Application\Handler;
 use App\Module\Series\Application\Command\AddEpisodeRating;
 use App\Module\Series\Domain\Repository\SeriesRepositoryInterface;
 use App\Module\Series\Domain\ValueObject\Rating;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\Attribute\Target;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Component\Messenger\MessageBusInterface;
@@ -17,6 +18,7 @@ final readonly class AddEpisodeRatingHandler
     public function __construct(
         private SeriesRepositoryInterface $repository,
         #[Target('event.bus')] private MessageBusInterface $eventBus,
+        #[Target('series')] private LoggerInterface $logger,
     ) {}
 
     public function __invoke(AddEpisodeRating $command): void
@@ -37,5 +39,11 @@ final readonly class AddEpisodeRatingHandler
         foreach ($series->releaseEvents() as $event) {
             $this->eventBus->dispatch($event);
         }
+
+        $this->logger->info('Episode rated', [
+            'seriesId' => $command->seriesId,
+            'episodeId' => $command->episodeId,
+            'rating' => $command->rating,
+        ]);
     }
 }
