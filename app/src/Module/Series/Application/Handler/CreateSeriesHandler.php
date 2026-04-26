743936a33c93f05fdbb1a31279dd8d9e2447dc98
Leszek Koziatek <poczta@leszekkoziatek.pl>
2026-04-26 20:38:10 +0200

    HMAI-12 - Graylog + New Relic — centralized logging and APM (#12)

--- app/src/Module/Series/Application/Handler/CreateSeriesHandler.php
+++ app/src/Module/Series/Application/Handler/CreateSeriesHandler.php
@@ -7,6 +7,8 @@ namespace App\Module\Series\Application\Handler;
 use App\Module\Series\Application\Command\CreateSeries;
 use App\Module\Series\Domain\Entity\Series;
 use App\Module\Series\Domain\Repository\SeriesRepositoryInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\Attribute\Target;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Component\Uid\Uuid;
 
@@ -15,6 +17,7 @@ final readonly class CreateSeriesHandler
 {
     public function __construct(
         private SeriesRepositoryInterface $repository,
+        #[Target('series')] private LoggerInterface $logger,
     ) {}
 
     public function __invoke(CreateSeries $command): string
@@ -22,6 +25,8 @@ final readonly class CreateSeriesHandler
         $id = Uuid::v4()->toRfc4122();
 
         $this->repository->save(new Series(id: $id, title: $command->title));
+
+        $this->logger->info('Series created', ['id' => $id, 'title' => $command->title]);
 
         return $id;
     }
